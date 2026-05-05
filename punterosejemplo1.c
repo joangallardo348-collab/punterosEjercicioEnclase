782b5aee753a571d591d371b9fc22f5801563d88
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 12:03:45 -0500

    se define el puntero de decimal

--- punterosejemplo1.c
+++ punterosejemplo1.c
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[]){
     int *x;
 
     x = &num; //x se le asigna la direccion de num
-    a = *x;
+    a = *x;   // a se le asigna el valor de x
     printf("%p %d\n", x, a);
 
 
@@ -14,7 +14,12 @@ int main(int argc, char const *argv[]){
     ptr = &letra;
     c = *ptr;
     printf("%p %c\n", ptr, c);
-    
+
+    float decimal = 3.14, dec;
+    float *ptrf;
+    ptrf = &decimal;
+    dec = *ptrf;
+    printf("%p %f", ptr, dec);
 
     return(0);
 }
